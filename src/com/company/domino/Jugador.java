c8132c8a0590140cf6c0a4357b0bc1234a62fcf9
Joan Vallbona <johnyvallbona@gmail.com>
2022-05-13 11:35:29 +0200

    Atributs i constructors de classe Jugador

--- src/com/company/domino/Jugador.java
+++ src/com/company/domino/Jugador.java
@@ -1,4 +1,19 @@
 package com.company.domino;
 
+import java.util.ArrayList;
+
 public class Jugador {
+
+    private int idJug;
+    private int idEquip;
+    private ArrayList<Fixa> fitxesJug;
+
+    public Jugador (int idJug){
+        this.idJug = idJug;
+    }
+
+    public Jugador (int idJug, int idEquip){
+        this.idJug = idJug;
+        this.idEquip = idEquip;
+    }
 }
