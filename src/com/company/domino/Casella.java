7af6154a47285828e55b55fcf3dcf624de6e4925
Joan Vallbona <johnyvallbona@gmail.com>
2022-05-13 12:14:05 +0200

    Atributs i constructor classe Tablero i Casella

---
+++ src/com/company/domino/Casella.java
@@ -0,0 +1,14 @@
+package com.company.domino;
+
+public class Casella {
+
+    private int x;
+    private int y;
+    private boolean ocupada;
+
+    public Casella (int x, int y){
+        this.x = x;
+        this.y = y;
+        this.ocupada = false;
+    }
+}
--- src/com/company/domino/Tablero.java
+++ src/com/company/domino/Tablero.java
@@ -1,4 +1,12 @@
 package com.company.domino;
 
 public class Tablero {
+
+    private int longitud;
+    private int altura;
+
+    public Tablero (int longitud, int altura){
+        this.longitud = 16;
+        this.altura = 16;
+    }
 }
