236202d737615c89d357a856939640e1ed0d0ace
Joan Vallbona <johnyvallbona@gmail.com>
2022-05-13 11:42:37 +0200

    Atributs i constructor de classe Equip

--- src/com/company/domino/Equip.java
+++ src/com/company/domino/Equip.java
@@ -1,4 +1,13 @@
 package com.company.domino;
 
+import java.util.ArrayList;
+
 public class Equip {
+
+    private int idEquip;
+    private ArrayList<Fixa> fitxesEquip;
+
+    public Equip (int idEquip){
+        this.idEquip = idEquip;
+    }
 }
