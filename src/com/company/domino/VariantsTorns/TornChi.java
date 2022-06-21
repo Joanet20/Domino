896046e2e12bf2f4788515d130f1787ae27b686f
Joan Vallbona <johnyvallbona@gmail.com>
2022-06-21 11:47:18 +0200

    Cuatre coses

--- src/com/company/domino/VariantsTorns/TornChi.java
+++ src/com/company/domino/VariantsTorns/TornChi.java
@@ -54,7 +54,7 @@ public class TornChi extends Torn{
                         newGame.posarExtremsTablero(tablero, player, player.getFitxesJug().size()-1);
                     }
                 }
-                Output.imprimirTablero(tablero, player.getFitxesJug().get(fitxaTriada));
+                //Output.imprimirTablero(tablero, player.getFitxesJug().get(fitxaTriada));
             }
         }
         seguentTorn(tornInicial, newGame.numeroJugadors(jugadors));
